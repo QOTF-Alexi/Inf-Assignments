1245484a53128062fdddcca5c82d19b3e56a97ea
Alexi <139751264+QOTF-Alexi@users.noreply.github.com>
2023-10-03 11:58:12 +0200

    try to satisfy Semgrep (I failed lol)

--- Codegrade/automatedArithmetics.py
+++ Codegrade/automatedArithmetics.py
@@ -1,9 +1,9 @@
 from random import randint
 
-generate_exercises = 10
+generate_exercises_amount = 10
 typeGiven = False
 
-while typeGiven == False:
+while typeGiven is False:
     arithmetic_type = str(input("Enter the arithmetic type (summation, multiplication, subtraction): "))
     if arithmetic_type == "summation" or arithmetic_type == "multiplication" or arithmetic_type == "subtraction":
         typeGiven = True
@@ -21,18 +21,18 @@ def arithmetic_operation(arithmetic_type):
         "subtraction": '-'
     }
     print("Arithmetic operation:", arithmetic_type)
-    for i in range(generate_exercises):
-        num1 = randint(1, 100)
-        num2 = randint(1, 100)
+    for exercise in range(generate_exercises_amount):
+        exerciseSummand1 = randint(1, 100)
+        exerciseSummand2 = randint(1, 100)
         operation = operationDict[arithmetic_type]
-        print(num1, operation, num2, "=", end='')
+        print(exerciseSummand1, operation, exerciseSummand2, "=", end='')
         userAns = int(input(" "))
         if operation == '+':
-            correctAns = num1+num2
+            correctAns = exerciseSummand1+exerciseSummand2
         elif operation == '*':
-            correctAns = num1*num2
+            correctAns = exerciseSummand1*exerciseSummand2
         elif operation == '-':
-            correctAns = num1-num2
+            correctAns = exerciseSummand1-exerciseSummand2
         else:
             print("How did I get here?")
         if correctAns == userAns:
@@ -42,5 +42,5 @@ def arithmetic_operation(arithmetic_type):
     print("You got", correct, "correct and", incorrect, "incorrect answers in", arithmetic_type)
 
 
-if __name__ == "__main__": # Only runs the function if the file is accessed directly.
+if __name__ == "__main__":      # Only runs the function if the file is accessed directly.
     arithmetic_operation(arithmetic_type)
