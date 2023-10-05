2b7ee28b1113fade4ec8913ec07b90720de67b44
Alexi <139751264+QOTF-Alexi@users.noreply.github.com>
2023-10-05 10:30:41 +0200

    prints full

--- Codegrade/12daysofChristmas.py
+++ Codegrade/12daysofChristmas.py
@@ -1,22 +1,22 @@
 gifts = [
-    'partridge in a pear tree', 'pipers piping', 'lords a-leaping',
-    'ladies dancing', 'maids a-milking', 'swans a-swimming',
-    'geese a-laying', 'gold rings', 'calling birds', 'French hens',
-    'turtle doves', 'drummers drumming'
+    "partridge in a pear tree", "turtledoves", "french hens",
+    "calling birds", "gold rings (five golden rings)", "geese a-laying",
+    "swans a-swimming", "maids a-milking", "ladies dancing",
+    "lords a-leaping", "pipers piping", "drummers drumming"
 ]
 
 numberToText = {
-    2: "two",
-    3: "three",
-    4: "four",
-    5: "five",
-    6: "six",
-    7: "seven",
-    8: "eight",
-    9: "nine",
-    10: "ten",
-    11: "eleven",
-    12: "twelve"
+    2: "Two",
+    3: "Three",
+    4: "Four",
+    5: "Five",
+    6: "Six",
+    7: "Seven",
+    8: "Eight",
+    9: "Nine",
+    10: "Ten",
+    11: "Eleven",
+    12: "Twelve"
 }
 
 
@@ -31,19 +31,19 @@ def next_verse(vers_number):
         else:
             verse = "On the " + str(day) + "th"
 
-    verse = verse + " day of Christmas my true love sent to me"
-    for giftNumber in range(day - 1 , -1, -1):  #Loops through the list of gifts, in reverse order
+    verse = verse + " day of Christmas, my true love sent to me"
+    for giftNumber in range(day - 1, -1, -1):  # Loops through the list of gifts, in reverse order
         if giftNumber == 0:
             if day == 1:
-                verse += " a " + gifts[giftNumber] + "."
+                verse += " A " + gifts[giftNumber]
             else:
                 verse = verse[:-1]  # remove the last comma
-                verse += " and a " + gifts[giftNumber] + "."
+                verse += " And A " + gifts[giftNumber]
         else:
             verse += " " + numberToText[(giftNumber + 1)] + " " + gifts[giftNumber] + ","
-    print(verse +"\n")
+    print(verse + "\n")
 
 
 if __name__ == "__main__":
-    vers_number = 3
-    next_verse(vers_number)
+    for complete in range(1, 13):
+        next_verse(complete)
