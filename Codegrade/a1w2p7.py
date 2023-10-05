9127633c77dd8a0baea2bba9cd843198c3080aa9
Alexi <139751264+QOTF-Alexi@users.noreply.github.com>
2023-10-05 15:03:52 +0200

    Satisfy Flake8

--- Codegrade/a1w2p7.py
+++ Codegrade/a1w2p7.py
@@ -1,5 +1,5 @@
 coordinate = input("Position: ")
-if (ord(coordinate[0]))%2 == int(coordinate[1])%2:
-	print("Black")
+if (ord(coordinate[0])) % 2 == int(coordinate[1]) % 2:
+    print("Black")
 else:
-	print("White")
+    print("White")
