90a1b6ae0b830d3842b137284927188bda27f707
Alexi <139751264+QOTF-Alexi@users.noreply.github.com>
2023-10-05 14:57:45 +0200

    Try to satisfy Flake8

--- Codegrade/processStudentCSV.py
+++ Codegrade/processStudentCSV.py
@@ -3,32 +3,14 @@ import sys
 
 valid_lines = []
 corrupt_lines = []
-
-'''
-The validate_data function will check the students.csv line by line for corrupt data.
-
-- Valid lines should be added to the valid_lines list.
-- Invalid lines should be added to the corrupt_lines list.
-
-Example input: 0896801,Kari,Wilmore,1970-06-18,INF
-This data is valid and the line should be added to the valid_lines list unchanged.
-
-Example input: 0773226,Junette,Gur_ry,1995-12-05,
-This data is invalid and the line should be added to the corrupt_lines list in the following format:
-
-0773226,Junette,Gur_ry,1995-12-05, => INVALID DATA: ['0773226', 'Gur_ry', '']
-
-In the above example the studentnumber does not start with '08' or '09',
-the last name contains a special character and the student program is empty.
-
-Don't forget to put the students.csv file in the same location as this file!
-'''
+invalid_data = []
 
 illegalChars = "1234567890,./<>?`~!@#$%^&*()_+=[]{}|;:\'\""
 validProgrammes = ["INF", "TINF", "CMD", "AI"]
 
 
 def validate_data(line):
+    invalid_data = []
     splitLine = line.split(sep=",")
     studNum = str(splitLine[0])
     firstName = str(splitLine[1])
@@ -45,35 +27,52 @@ def validate_data(line):
         studNumValid = True
     else:
         studNumValid = False
+        invalid_data.append(studNum)
 
     if not any(element in firstName for element in illegalChars):
-        if not any(element in lastName for element in illegalChars):
-            nameValid = True
-        else:
-            nameValid = False
+        firstNameValid = True
     else:
-        nameValid = False
+        firstNameValid = False
+        invalid_data.append(firstName)
+
+    if not any(element in lastName for element in illegalChars):
+        lastNameValid = True
+    else:
+        lastNameValid = False
+        invalid_data.append(lastName)
 
     if studProgramme in validProgrammes:
         studProgrammeValid = True
     else:
         studProgrammeValid = False
+        invalid_data.append(studProgramme)
 
     if 1960 <= birthYear <= 2004:
-        if 1 <= birthMonth <= 12:
-            if 1 <= birthDay <= 31:
-                birthDateValid = True
-            else:
-                birthDateValid = False
-        else:
-            birthDateValid = False
+        birthYearValid = True
+    else:
+        birthYearValid = False
+
+    if 1 <= birthMonth <= 12:
+        birthMonthValid = True
+    else:
+        birthMonthValid = False
+
+    if 1 <= birthDay <= 31:
+        birthDayValid = True
+    else:
+        birthDayValid = False
+
+    if birthYearValid and birthMonthValid and birthDayValid:
+        birthDateValid = True
     else:
         birthDateValid = False
+        invalid_data.append(splitLine[3])
 
-    if studNumValid and nameValid and studProgrammeValid and birthDateValid:
+    if studNumValid and firstNameValid and lastNameValid and studProgrammeValid and birthDateValid:
         valid_lines.append(",".join(str(element) for element in splitLine))
     else:
-        corrupt_lines.append(",".join(str(element) for element in splitLine))
+        corrupt_lines.append(str(",".join(str(element) for element in splitLine)) +
+                             " => INVALID DATA: " + str(invalid_data))
 
 
 def main(csv_file):
@@ -90,5 +89,5 @@ def main(csv_file):
     print("\n".join(corrupt_lines))
 
 
-if __name__ == "__main__":    
+if __name__ == "__main__":
     main('students.csv')
