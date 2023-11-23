513bb0e45f1d23acf5863c804d0dc7f2a6201e0e
Alexi <139751264+QOTF-Alexi@users.noreply.github.com>
2023-11-23 12:28:29 +0100

    add error resilience

--- Codegrade/automatedArithmetics.py
+++ Codegrade/automatedArithmetics.py
@@ -12,33 +12,46 @@ while typeGiven is False:
         typeGiven = False
 
 
-def arithmetic_operation(arithmetic_type):
-    correct = 0
-    incorrect = 0
+def generateProblem(arithmetic_type):
     operationDict = {
         "summation": '+',
         "multiplication": '*',
         "subtraction": '-'
     }
+
+    exerciseSummand1 = randint(1, 100)
+    exerciseSummand2 = randint(1, 100)
+    operation = operationDict[arithmetic_type]
+
+    if operation == '+':
+        correctAns = exerciseSummand1+exerciseSummand2
+    elif operation == '*':
+        correctAns = exerciseSummand1*exerciseSummand2
+    elif operation == '-':
+        correctAns = exerciseSummand1-exerciseSummand2
+    else:
+        print("How did I get here?")
+    print(exerciseSummand1, operation, exerciseSummand2, "=", end='')
+    return correctAns
+
+
+def arithmetic_operation(arithmetic_type):
+    correct = 0
+    incorrect = 0
     print("Arithmetic operation:", arithmetic_type)
     for exercise in range(generate_exercises_amount):
-        exerciseSummand1 = randint(1, 100)
-        exerciseSummand2 = randint(1, 100)
-        operation = operationDict[arithmetic_type]
-        print(exerciseSummand1, operation, exerciseSummand2, "=", end='')
-        userAns = int(input(" "))
-        if operation == '+':
-            correctAns = exerciseSummand1+exerciseSummand2
-        elif operation == '*':
-            correctAns = exerciseSummand1*exerciseSummand2
-        elif operation == '-':
-            correctAns = exerciseSummand1-exerciseSummand2
-        else:
-            print("How did I get here?")
-        if correctAns == userAns:
-            correct += 1
-        else:
-            incorrect += 1
+        correctAns = generateProblem(arithmetic_type)
+        intGiven = False
+        while not intGiven:
+            try:
+                userAns = int(input(" "))
+                if correctAns == userAns:
+                    correct += 1
+                else:
+                    incorrect += 1
+                intGiven = True
+            except ValueError:
+                print("That is not a number!")
     print("You got", correct, "correct and", incorrect, "incorrect answers in", arithmetic_type)
 
 
