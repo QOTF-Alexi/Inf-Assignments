3249cda7581228e1ccdc61231fdb8abac44ccf95
Alexi <139751264+QOTF-Alexi@users.noreply.github.com>
2023-12-05 15:51:50 +0100

    provide base structure

--- Codegrade/carparkingreports.py
+++ Codegrade/carparkingreports.py
@@ -1,3 +1,42 @@
+from datetime import datetime
+import os
+import sys
+import csv
+
+
+def reportCars():
+    requirements = input("car parking machine identifier, from date, to date (date format: DD-MM-YYYY): ")
+    requirementsList = requirements.split(sep=",")
+    cpm = requirementsList[0]
+    fromDate = requirementsList[1]
+    toDate = requirementsList[2]
+    fname = ".".join(requirementsList)      # Provide a base file name
+
+    parkedHere = []
+
+    with open("carparklog.txt", "r", encoding="utf8") as logFile:
+        for record in logFile:
+            recordList = record.split(sep=";")      # Separates all items in a record.
+            if fromDate < recordList[0] < toDate and recordList[1] == cpm:
+                pass    # IMPLEMENT
+
+    with open(f"{fname}.car_report.csv", "a", encoding="utf8") as file:
+        file.write("license_plate;check-in;check-out;parking_fee")
+    pass
+
+
+def reportFees():
+    requirements = input("from date, to date (date format: DD-MM-YYYY): ")
+    requirementsList = requirements.split(sep=",")
+    fname = ".".join(requirementsList)      # Provide a base file name
+
+    # Calculate collected fees between given dates.
+
+    with open(f"cpms{fname}.fee_report.csv", "a", encoding="utf8") as file:
+        file.write("car_parking_machine;total_parking_fee")
+    pass
+
+
 def main():
     running = True
     while running:
@@ -6,9 +45,9 @@ def main():
         print("[Q] Quit program")
         choice = input("What would you like to do? ").upper()
         if choice == "P":
-            pass
+            reportCars()
         elif choice == "F":
-            pass
+            reportFees()
         elif choice == "Q":
             running = False
         else:
@@ -16,4 +55,4 @@ def main():
 
 
 if __name__ == "__main__":
-    pass
+    main()
