7ddf04beb5762ab32b4b5b18e0f3c33c15dc3bda
Alexi <139751264+QOTF-Alexi@users.noreply.github.com>
2023-12-12 09:50:13 +0100

    i hate u semgrep

--- Codegrade/sortByVowels.py
+++ Codegrade/sortByVowels.py
@@ -1,15 +1,15 @@
 def get_num_of_vowels(inp: str) -> int:
-  vowel_count = 0
-  vowels = 'aeiou'
-  for char in inp:
-    if char in vowels:
-      vowel_count += 1
-  return vowel_count
+    vowel_count = 0
+    vowels = 'aeiou'
+    for char in inp:
+        if char in vowels:
+            vowel_count += 1
+    return vowel_count
 
 
 def sort_basedon_vowels():
-  cases = ['code', 'programming', 'description', 'fly', 'free']
-  print(sorted(cases, key=lambda word: sum(ch in 'aeiou' for ch in word)))
+    cases = ['code', 'programming', 'description', 'fly', 'free']
+    print(sorted(cases, key=lambda word: get_num_of_vowels(word)))
 
 
 if __name__ == "__main__":
